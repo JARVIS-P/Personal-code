abfc4dc18eedfdf51823ec50852e4fb1bc0492a3
yangxu <1938796569@qq.com>
2020-08-16 21:05:39 +0800

    UICustomViews

---
+++ UICustomViews/app/src/main/java/com/example/uicustomviews/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.uicustomviews;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar=getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.hide();
+        }
+    }
+}
