a4a869b0697e94782022cbd2fdc3e55ea8dbdfdb
yangxu <1938796569@qq.com>
2020-08-17 11:15:44 +0800

    ListViewTest

---
+++ ListViewTest/app/src/main/java/com/example/listviewtest/FruitAdapter.java
@@ -0,0 +1,54 @@
+package com.example.listviewtest;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class FruitAdapter extends ArrayAdapter<Fruit> {
+    private int resourceId;
+
+    public FruitAdapter(@NonNull Context context,int textViewResourceId, @NonNull List<Fruit> objects) {
+        super(context, textViewResourceId, objects);
+        resourceId=textViewResourceId;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Fruit fruit=getItem(position);//获取当前Fruit实例
+        View view;
+        ViewHolder viewHolder;
+        if(convertView==null){
+            view=LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
+            viewHolder=new ViewHolder();
+            viewHolder.fruitImage=(ImageView) view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName=(TextView) view.findViewById(R.id.fruit_name);
+            view.setTag(viewHolder);//将ViewHolder存储在View中
+        }else {
+            view=convertView;
+            viewHolder=(ViewHolder) view.getTag();//重新获取ViewHolder
+        }
+        /* ImageView fruitImage=(ImageView) view.findViewById(R.id.fruit_image);
+        TextView fruitName=(TextView) view.findViewById(R.id.fruit_name);
+        fruitImage.setImageResource(fruit.getImageId());
+        fruitName.setText(fruit.getName());*/
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
+        return view;
+    }
+
+    class ViewHolder{
+        ImageView fruitImage;
+
+        TextView fruitName;
+    }
+}
